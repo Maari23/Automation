dab9d7c4f05a4b78a46b9547c0aaa07f69150216
kumarsharath00oo7@gmail.com <kumarsharath00oo7@gmail.com>
2023-06-21 21:15:20 +0530

    new commit

---
+++ src/test/java/com/gitDemo/Grid/Teest.java
@@ -0,0 +1,30 @@
+package com.gitDemo.Grid;
+
+import java.net.MalformedURLException;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class Teest {
+	
+	@Test
+	public void Checkin() throws MalformedURLException 
+	{
+		WebDriver d=new ChromeDriver();
+	d.get("http://google.com");
+	d.manage().window().maximize();
+	d.close();
+	System.out.println("test");
+	System.out.println("test2");
+	System.out.println("test2");
+
+
+	
+	}
+
+}
